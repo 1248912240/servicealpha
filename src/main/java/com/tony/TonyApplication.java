28175b55df86233d460a12376ae4cc2353f04614
fut <1248912240@qq.com>
2018-11-26 22:30:24 +0800

    service one created

--- src/main/java/com/tony/TonyApplication.java
+++ src/main/java/com/tony/TonyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @Author fut
  * @Date 2018-11-26
- * @Description TODO
+ * @Description
  * @Wiki:
  */
 @SpringBootApplication
