7e5d051be68869bd143f0cedd05fbf9ec4b4fe30
fut <1248912240@qq.com>
2018-11-26 22:29:45 +0800

    service one created

---
+++ src/main/java/com/tony/TonyApplication.java
@@ -0,0 +1,19 @@
+package com.tony;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author fut
+ * @Date 2018-11-26
+ * @Description TODO
+ * @Wiki:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TonyApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TonyApplication.class).web(true).run(args);
+    }
+}
---
+++ src/main/java/com/tony/controller/AlphaController.java
@@ -0,0 +1,23 @@
+package com.tony.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author fut
+ * @Date 2018-11-26
+ * @Description TODO
+ * @Wiki:
+ */
+@RestController
+@RequestMapping("alpha")
+public class AlphaController {
+
+    @RequestMapping("/about")
+    public String getAlpha() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
+    }
+}
